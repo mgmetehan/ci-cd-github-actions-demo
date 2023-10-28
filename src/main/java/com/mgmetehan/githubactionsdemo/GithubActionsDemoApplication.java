ad79e3b6ef45f9adbf9b432e0154483ae662201f
mgmetehan <mgmetehan@hotmail.com>
2023-10-28 18:05:56 +0300

    init project

---
+++ src/main/java/com/mgmetehan/githubactionsdemo/GithubActionsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.githubactionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgmetehan/githubactionsdemo/GithubActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.githubactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
